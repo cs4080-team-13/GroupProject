a272965089a9729388e6264ae2babd533439d520
TheDrGoldeneye <33264484+TheDrGoldeneye@users.noreply.github.com>
2019-05-01 15:01:24 -0700

    Tweaked user interface and added icon

--- CS4080_Project/CS4080_Project/My Project/Application.Designer.vb
+++ CS4080_Project/CS4080_Project/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CS4080_Project.frmFinanceWizard
+            Me.MainForm = Global.CS4080_Project.frmMortgageCalculator
         End Sub
     End Class
 End Namespace
