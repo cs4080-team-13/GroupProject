d3df63d3dcb88421b683e6841cb113aee6694ab5
TheDrGoldeneye <33264484+TheDrGoldeneye@users.noreply.github.com>
2019-04-30 17:08:11 -0700

    Simplified code, updated mathematical errors
    produces reasonable results but still needs mathematical corrections

--- CS4080_Project/CS4080_Project/financialWIzard.vb
+++ CS4080_Project/CS4080_Project/financialWIzard.vb
@@ -1,26 +1,13 @@
 ﻿Public Class frmFinanceWizard
     'Get all the data from the user
-    Dim decLoanAmount As Decimal
-    Dim decLoanFees As Decimal
-    Dim decInterestRate As Decimal
+    Private decMonthlyPayment As Decimal
+    Private decLoanAmount As Decimal
+    Private decLoanFees As Decimal
+    Private decInterestRate As Decimal
     Dim intMortgageTerm As Integer
-
-    Private Sub MonthCalendar1_DateChanged(sender As Object, e As DateRangeEventArgs)
-
-    End Sub
-
-    Private Sub btnCalendar_Click(sender As Object, e As EventArgs)
-        panelExpenseTracker.Visible = False
-        panelMortgageCalc.Visible = False
-    End Sub
 
     Private Sub btnExpense_Click(sender As Object, e As EventArgs) Handles btnExpense.Click
         panelExpenseTracker.Visible = True
-        panelMortgageCalc.Visible = False
-    End Sub
-
-    Private Sub btnInvestment_Click(sender As Object, e As EventArgs)
-        panelExpenseTracker.Visible = False
         panelMortgageCalc.Visible = False
     End Sub
 
@@ -31,7 +18,7 @@
 
     'Calculates the number mortgage payment amount per month for a specified loan amount, annual interest rate, and mortgage term
     Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
-
+        lsvMonthlySchedule.Clear()
         Try
             decLoanAmount = Decimal.Parse(txtbxLoanAmount.Text)
             decLoanFees = Double.Parse(txtbxLoanFees.Text)
@@ -41,27 +28,34 @@
             MsgBox("Please use only numerical digits", MsgBoxStyle.OkOnly, "Error")
             Return
         End Try
-        'Begin calculating the montly payment, total interest paid, total paid
+        'Calculate the montly interest rate
         decInterestRate = decInterestRate / 100
-        Dim decMonthlyPayment As Decimal
-        Dim decInterestPaid As Decimal
-        Dim decTotalPaid As Decimal
-        Dim decMonthlyInterestRate As Decimal = (decInterestRate / 12)
+        Dim decMonthlyInterest As Decimal
+        Dim decMonthlyInterestRate As Decimal = decInterestRate / 12
         Dim intMonths As Integer = intMortgageTerm * 12
-        Dim decBalance As Decimal
-        decLoanAmount = (decLoanAmount + decLoanFees)
+        Dim decBalance As Decimal = decLoanAmount + decLoanFees
+        'Calulate the monthly payment, it only needs to be calculated once
+        decMonthlyPayment = calcMonthlyPayment(decMonthlyInterestRate, intMonths)
         For i As Integer = 1 To intMonths
-            decMonthlyPayment = decLoanAmount * (decMonthlyInterestRate * (1 + decMonthlyInterestRate) ^ intMonths) / ((1 + decMonthlyInterestRate) ^ intMonths - 1)
-            decBalance = decLoanAmount * (decMonthlyInterestRate * (((1 + decMonthlyInterestRate) ^ intMonths) - (1 + decMonthlyInterestRate) ^ decMonthlyPayment)) / ((1 + decMonthlyInterestRate) ^ intMonths - 1)
-            decInterestPaid = decLoanAmount * decMonthlyInterestRate
+            'Calculate the interest for month i
+            decMonthlyInterest = Math.Round((decBalance * decMonthlyInterestRate), 2)
+            'Create an array of strings to be added to the ListView
             Dim strMonthInfo(5) As String
             strMonthInfo(0) = i.ToString
-            strMonthInfo(1) = FormatNumber(decLoanAmount, 2)
-            strMonthInfo(2) = FormatNumber(decInterestPaid, 2)
+            strMonthInfo(1) = FormatNumber(decBalance, 2)
+            strMonthInfo(2) = FormatNumber(decMonthlyInterest, 2)
             strMonthInfo(3) = FormatNumber(decMonthlyPayment, 2)
+            'Calculate the remaining balance
+            decBalance = calcBalance(decBalance, decMonthlyInterest)
             strMonthInfo(4) = FormatNumber(decBalance, 2)
+            'Pass the string to the ListView item
             lsvMonthlySchedule.Items.Add(New ListViewItem(strMonthInfo))
-            decLoanAmount = decBalance
         Next
     End Sub
+    Private Function calcMonthlyPayment(ByVal decMIR As Decimal, ByVal intM As Integer) As Decimal
+        Return Math.Round((decLoanAmount * (decMIR * (1 + decMIR) ^ intM) / (((1 + decMIR) ^ intM) - 1)), 2)
+    End Function
+    Private Function calcBalance(ByVal decBal As Decimal, ByVal decMI As Decimal) As Decimal
+        Return Math.Round((decBal - (decMonthlyPayment - decMI)))
+    End Function
 End Class
